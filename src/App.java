3e805ace5e979d81db9c27a4b1deb05cf265018f
Aji Hamdani Ahmad <dani707hamdani@gmail.com>
2023-02-06 07:48:13 +0700

    adding style GUI

--- src/App.java
+++ src/App.java
@@ -1,46 +1,33 @@
 import com.config.Config;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.Border;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+// pallete
 import java.awt.Color;
-import javax.swing.BorderFactory;
+import java.awt.Font;
+// Event 
+import java.util.EventListener;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // code start from here
         JFrame frameWindow = new JFrame();
-
-        // setSize(Dimension d), setSize(int width, int height) -> inherited from class
-        // java.awt.Window
         frameWindow.setSize(1280, 720);
-
-        // setTitle("Title") -> inherited from class java.awt.Frame
         frameWindow.setTitle("Kicuy Shop");
-
-        // EXIT PROGRAM when close button clicked
+        frameWindow.setResizable(false);
         frameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         frameWindow.setLayout(null);
 
         // Panel background
         JPanel bg = new JPanel();
-
-        // inherited from class java.awt.Component
         bg.setSize(1280, 720);
-
-        // setBackground(Dimension / width & height) -> inherited javax.swing.JComponent
-        // throw argumen object Color
         bg.setBackground(new Color(255, 255, 255));
-
-        // setLayout
         bg.setLayout(null);
-
-        // add bg to frame
         frameWindow.add(bg);
 
-        // Panel SideBar
         JPanel sideBar = new JPanel();
         sideBar.setSize(256, 720);
         sideBar.setBackground(new Color(59, 59, 59));
@@ -50,10 +37,60 @@ public class App {
         // Panel Navbar
         JPanel navbar = new JPanel();
         navbar.setSize(1024, 96);
-        navbar.setBackground(new Color(194,194,194));
+        navbar.setBackground(new Color(194, 194, 194));
         navbar.setLocation(256, 0);
         navbar.setLayout(null);
         bg.add(navbar);
+
+        // button get all data
+        JButton btnGetAllData = new JButton("Query All Data");
+        btnGetAllData.setBounds(10, 100, 230, 80);
+        btnGetAllData.setBackground(new Color(120, 120, 120));
+        sideBar.add(btnGetAllData);
+
+        // btn search data
+        JButton btnSearchData = new JButton("Search Data");
+        btnSearchData.setBounds(10, (int) btnGetAllData.getLocation().getY() +
+                btnGetAllData.getHeight() + 30, 230, 80);
+        btnSearchData.setBackground(new Color(120, 120, 120));
+        sideBar.add(btnSearchData);
+
+        // btn update data
+        JButton btnUpdateData = new JButton("Update Data");
+        btnUpdateData.setBounds(10, (int) btnSearchData.getLocation().getY() +
+                btnSearchData.getHeight() + 30, 230, 80);
+        btnUpdateData.setBackground(new Color(120, 120, 120));
+        sideBar.add(btnUpdateData);
+
+        // btn delete data
+        JButton btnDeleteData = new JButton("Delete Data");
+        btnDeleteData.setBounds(10, (int) btnUpdateData.getLocation().getY() +
+                btnUpdateData.getHeight() + 30, 230, 80);
+        btnDeleteData.setBackground(new Color(120, 120, 120));
+        sideBar.add(btnDeleteData);
+
+        // btn logout
+        JButton btnOut = new JButton("Keluar");
+        btnOut.setBounds(10, (int) btnDeleteData.getLocation().getY() +
+                btnDeleteData.getHeight() + 30, 230, 80);
+        btnOut.setBackground(new Color(120, 120, 120));
+        sideBar.add(btnOut);
+
+        // navbar
+        JLabel logo = new JLabel("KICUY SHOP MANAGEMENT");
+        logo.setBounds(20, 0, 400, 100);
+        logo.setFont(new Font("Arial", Font.BOLD, 20));
+        navbar.add(logo);
+
+        // panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBounds(256, 96, 1024, 624);
+        mainPanel.setBackground(new Color(196, 213, 229));
+        JLabel txtDashboard = new JLabel("Dashboard");
+        txtDashboard.setFont(new Font("Arial", Font.PLAIN, 40));
+        txtDashboard.setBounds(20, 20, 100, 50);
+        mainPanel.add(txtDashboard);
+        bg.add(mainPanel);
 
         // setVisible to set visibility (true) -> visible, (false) -> hidden
         frameWindow.setVisible(true);
